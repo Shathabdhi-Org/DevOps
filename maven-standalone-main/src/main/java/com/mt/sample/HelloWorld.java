a04bd9983f13ece30adfe2f7a5f659d07ff07b6b
Shathabdhi-DevOps <shathabdhi06@gmail.com>
2025-02-24 08:17:31 +0530

    Add files via upload

---
+++ maven-standalone-main/src/main/java/com/mt/sample/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.mt.sample;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello welcome to devops classes,we are lerning jenkins CI-CD Tool with demo of github webhook mew server again!! Today Date is:  " + getLocalCurrentDate());
+
+	}
+
+	private static Date getLocalCurrentDate() {
+		String m = "techno smart";
+		return new Date();
+
+	}
+
+}
